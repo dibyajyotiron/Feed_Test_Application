e70f645f7a86fe838306a876ee2edb5e1dc86990
Dibyajyoti <dibyajyoti.beingmyself.ghosal@gmail.com>
2019-04-04 17:12:42 +0530

    changed middleware logic

--- middlewares/commentMiddleware.js
+++ middlewares/commentMiddleware.js
@@ -1,5 +1,5 @@
 const { Comment, validate } = require("../models/comment");
-const { isEqual } = require("lodash");
+
 module.exports = {
   validateCommentBody: (req, res, next) => {
     const { error } = validate(req.body);
@@ -12,20 +12,17 @@ module.exports = {
   },
   validateComment: async (req, res, next) => {
     let comment;
+    const { commentUID } = req.params;
+    comment = await Comment.findOne({ uid: commentUID });
 
-    if (req.body._parentCommentUid) {
-      const { _parentCommentUid } = req.body;
-      comment = await Comment.findOne({ uid: _parentCommentUid });
-    }
-    if (req.params.commentUID) {
-      const { commentUID } = req.params;
-      comment = await Comment.findOne({ uid: commentUID });
-    }
+    const url = req.url.split("/").filter(u => u.length > 0);
+
     if (!comment)
       return res.status(400).json({
         error: true,
-        message: "The comment you're trying to reply to is not there anymore!"
+        message: `The comment you're trying to ${!url.includes("vote") ? "reply to" : "vote"} is not there anymore!`
       });
+
     res.locals.comment = comment;
     return next();
   }
