00d8693af62c60d1e337f78ae9a230c53cc7a793
Dibyajyoti <dibyajyoti.beingmyself.ghosal@gmail.com>
2019-04-26 18:22:04 +0530

    added testing env

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,17 @@
-if (process.env.NODE_ENV === 'development') {
-    module.exports = require('./dev');
-} else {
-    module.exports = require('./prod');
+switch (process.env.NODE_ENV) {
+  case "production":
+    module.exports = require("./prod");
+    break;
+  case "testing":
+    module.exports = require("./test");
+    break;
+  default:
+    module.exports = require("./dev");
+    break;
 }
+// if (process.env.NODE_ENV === "development") {
+// } else if (process.env.NODE_ENV === "testing") {
+//   module.exports = require("./test");
+// } else {
+//   module.exports = require("./prod");
+// }
