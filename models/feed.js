db490bdf05881282337c195a6ffae60c7f93ca08
Dibyajyoti <dibyajyoti.beingmyself.ghosal@gmail.com>
2019-04-30 12:03:36 +0530

    updated feed model

--- models/feed.js
+++ models/feed.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
-const Joi = require("joi");
+const uuid = require("uuid/v1");
 
 const { Schema } = mongoose;
 
 const { user } = require("./mixin");
+const { targetElementToElementPlugin } = require("./plugins");
 
 const feedSchema = new Schema(
   {
@@ -11,8 +12,13 @@ const feedSchema = new Schema(
     uid: String,
     description: String,
     owner: user,
-    _element: { type: String, required: true },
-    _appUid: { type: String, required: true },
+    _element: { type: Schema.Types.ObjectId, ref: "element" },
+    targetElementType: String,
+    targetElementUid: String,
+    targetElementStage: {
+      type: String,
+      enum: ["Therm", "Eye", "Terra", "Core"]
+    },
     data: [Object],
     readUsers: { type: [user] },
     writeUsers: { type: [user] },
@@ -26,52 +32,22 @@ const feedSchema = new Schema(
   },
   { timestamps: true }
 );
+feedSchema.plugin(targetElementToElementPlugin);
 
-function validate(feed) {
-  const schema = {
-    name: Joi.string().max(100),
-    description: Joi.string().max(500),
-    _appUid: Joi.string().required(),
-    _element: Joi.string().required(),
-    readUsers: getReadWriteValidSchema("read"),
-    writeUsers: getReadWriteValidSchema("write"),
-    data: Joi.array().items(Joi.object()),
-    labelsRead: Joi.array().items(Joi.string()),
-    labelsWrite: Joi.array().items(Joi.string()),
-    properties: Joi.object()
-  };
-  return Joi.validate(feed, schema);
-}
-
-function getUserValidSchema(perm) {
-  return {
-    uid: Joi.string()
-      .required()
-      .error(new Error(`${perm} user uid of type 'String' is required!`)),
-    email: Joi.string()
-      .email()
-      .required()
-      .error(new Error(`${perm} user email of type 'email' is required!`))
-  };
-}
-function getReadWriteValidSchema(perm) {
-  return perm === "read"
-    ? Joi.array()
-        .items(Joi.object().keys(getUserValidSchema("read")))
-        .min(1)
-    : Joi.array()
-        .items(Joi.object().keys(getUserValidSchema("write")))
-        .min(1);
-}
-
-feedSchema.pre("save", function(next) {
+feedSchema.pre("save", function(next, req) {
   if (!this.writeUsers.some(user => user.uid === this.owner.uid)) {
     this.writeUsers.push({ uid: this.owner.uid, email: this.owner.email });
+    this.uid = uuid();
+    this._element = req.newElement ? req.newElement._id : this._element;
   }
   return next();
 });
 
+feedSchema.pre("find", function(next) {
+  this.populate({
+    path: "_element"
+  });
+  next();
+});
+
 module.exports.Feed = mongoose.model("Feed", feedSchema);
-module.exports.validate = validate;
-module.exports.getUserValidSchema = getUserValidSchema;
-module.exports.getReadWriteValidSchema = getReadWriteValidSchema;
