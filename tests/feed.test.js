c735c4e21aeeaf7ac46fbab0361a0916ac2c66bb
Dibyajyoti <dibyajyoti.beingmyself.ghosal@gmail.com>
2019-05-02 15:21:35 +0530

    updated test logic

--- tests/feed.test.js
+++ tests/feed.test.js
@@ -3,36 +3,32 @@ process.env.NODE_ENV = "testing";
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
-const nock = require("nock");
 const request = require("supertest");
 
 const { expect } = require("chai");
 const colors = require("colors");
 
-const { Feed } = require("../models/feed");
-
-const baseURL = require("../config/keys").baseURL;
-const mongoURI = require("../config/keys").mongoURI;
-const validToken = require("../config/keys").validToken;
+const { baseURL } = require("../config/keys");
+const { mongoURI } = require("../config/keys");
+const { validToken } = require("../config/keys");
 
 const logger = require("../services/logger");
-
-let relativeURL;
 
 const variables = {
   feedUID: "703db4d0-5087-11e9-bdb1-357aaf6bbbf5"
 };
-
 describe("Feed routes".blue, () => {
   before(done => {
     // making db connection
-    mongoose.connect(mongoURI, { useNewUrlParser: true, useCreateIndex: true });
     const db = mongoose.connection;
+    mongoose.connect(mongoURI, { useNewUrlParser: true, useCreateIndex: true });
     db.on("error", logger.error.bind(console.name, "error"));
     db.once("open", () => {
       logger.info(`connected to ${mongoURI}`);
       done();
     });
+
+    //remove residue
     // insert data into db
     require("../specs/helpers/populate_data/populate")(db);
   });
@@ -43,52 +39,108 @@ describe("Feed routes".blue, () => {
       mongoose.connection.close(done);
     });
   });
-  describe("GET /feeds", () => {
-    it("should get all the feeds", async () => {
-      relativeURL = "feeds";
 
-      // const response = await nock(baseURL, { reqheaders: { authorization: validToken } })
-      //   .get(relativeURL)
-      //   .reply(200, allFeeds);
-      const response = await request(baseURL)
-        .get(relativeURL)
-        .set("Content-Type", "application/json")
-        .set("Authorization", validToken);
-      expect(response.status).to.equals(200);
-    });
-  });
-  describe("GET /feeds/:id", () => {
-    it("should get a feed by uid", async () => {
-      relativeURL = `feeds/${variables.feedUID}`;
-      const response = await request(baseURL)
-        .get(relativeURL)
-        .set("Authorization", validToken);
+  const testCases = [
+    {
+      desc: "GET /feeds",
+      motive: "should get all the feeds",
+      relativeURL: "feeds",
+      token: validToken,
+      matcher: {
+        length: 4
+      }
+    },
+    {
+      desc: "GET /feeds/:uid",
+      motive: "should get a particular feed",
+      relativeURL: `feeds/${variables.feedUID}`,
+      token: validToken,
 
-      expect(response.status).to.be.equals(200);
-      expect(response.body.uid).to.be.equals("703db4d0-5087-11e9-bdb1-357aaf6bbbf5");
-      expect(response.body.owner.uid).to.be.equals("00uhtrlx9cXkw9r3l0h7");
-    });
-  });
-  describe("POST /feeds/", () => {
-    it("should create a feed", async () => {
-      relativeURL = "feeds";
-      const response = await request(baseURL)
-        .post(relativeURL)
-        .set("Authorization", validToken)
-        .send({
-          name: "An element oriented Feed",
-          description: "new Feed",
-          targetElementType: "group",
-          targetElementUid: "12sdsdsa-asdsa3-sadsad-sweq",
-          targetElementStage: "Therm",
-          readUsers: [{ uid: "00uhtrlx9cXkw9r3l0h4", email: "user4@scm.com" }, { uid: "00uhtrlx9cXkw9r3l0h4", email: "user4@scm.com" }],
-          writeUsers: [{ uid: "00uhtrlx9cXkw9r3l0h8", email: "user2@scm.com" }, { uid: "00uhtrlx9cXkw9r3l0h9", email: "user3@scm.com" }],
-          labelsRead: ["labels1", "labels2", "labels2"],
-          labelsWrite: ["label", "label1"]
-        });
+      matcher: {
+        uid: variables.feedUID
+      }
+    },
+    {
+      desc: "POST /feeds",
+      motive: "should create a new feed",
+      relativeURL: "feeds",
+      token: validToken,
+      body: {
+        name: "An element oriented Feed",
+        description: "new Feed",
+        targetElementType: "group",
+        targetElementUid: "12sdsdsa-asdsa3-sadsad-sweq",
+        targetElementStage: "Therm",
+        readUsers: [{ uid: "00uhtrlx9cXkw9r3l0h4", email: "user4@scm.com" }, { uid: "00uhtrlx9cXkw9r3l0h4", email: "user4@scm.com" }],
+        writeUsers: [{ uid: "00uhtrlx9cXkw9r3l0h8", email: "user2@scm.com" }, { uid: "00uhtrlx9cXkw9r3l0h9", email: "user3@scm.com" }],
+        labelsRead: ["labels1", "labels2", "labels2"],
+        labelsWrite: ["label", "label1"]
+      },
+      matcher: {
+        success: true
+      }
+    },
+    {
+      desc: "PUT /feeds/:uid",
+      motive: "should update a feed",
+      relativeURL: `feeds/${variables.feedUID}`,
+      token: validToken,
+      body: {
+        name: "updated test feed",
+        description: "test Feed",
+        targetElementType: "group",
+        targetElementUid: "12sdsdsa-asdsa3-sadsad-sweq",
+        targetElementStage: "Therm",
+        readUsers: [{ uid: "00uhtrlx9cXkw9r3l0h4", email: "user4@scm.com" }, { uid: "00uhtrlx9cXkw9r3l0h4", email: "user4@scm.com" }],
+        writeUsers: [{ uid: "00uhtrlx9cXkw9r3l0h8", email: "user2@scm.com" }, { uid: "00uhtrlx9cXkw9r3l0h9", email: "user3@scm.com" }],
+        labelsRead: ["labels1", "labels2", "labels2"],
+        labelsWrite: ["label", "label1"]
+      },
+      matcher: {
+        success: true
+      }
+    }
+  ];
 
-      expect(response.status).to.be.equals(200);
-      expect(response.body.success).to.be.equals(true);
+  for (let testCase of testCases) {
+    describe(testCase.desc, () => {
+      it(testCase.motive, async () => {
+        let response;
+
+        switch (testCase.desc.split(" ")[0]) {
+          case "GET":
+            response = await request(baseURL)
+              .get(testCase.relativeURL)
+              .set("Authorization", testCase.token);
+            break;
+          case "POST":
+            response = await request(baseURL)
+              .post(testCase.relativeURL)
+              .set("Authorization", testCase.token)
+              .send(testCase.body);
+            break;
+
+          case "PUT":
+            response = await request(baseURL)
+              .put(testCase.relativeURL)
+              .set("Authorization", testCase.token);
+            break;
+          case "DELETE":
+            response = await request(baseURL)
+              .delete(testCase.relativeURL)
+              .set("Authorization", testCase.token);
+            break;
+        }
+        console.log(await request(baseURL).get(testCase.relativeURL));
+        console.log(baseURL, testCase.relativeURL);
+        expect(response.status).to.equals(200);
+        if (Array.isArray(response.body)) {
+          expect(response.body.length).to.equals(testCase.matcher.length);
+        }
+        for (let property in testCase.matcher) {
+          expect(response.body[property]).to.equals(testCase.matcher[property]);
+        }
+      });
     });
-  });
+  }
 });
