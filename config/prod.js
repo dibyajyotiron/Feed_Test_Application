4ef0238144550920c41e84818ae10f8104dcda4a
Dibyajyoti <dibyajyoti.beingmyself.ghosal@gmail.com>
2019-05-22 19:46:13 +0530

    added baseurl

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,9 @@
 module.exports = {
+  baseURL: "https://feed-prod.herokuapp.com/",
   mongoURI: process.env.mongoURI,
   okta: {
     url: process.env.OKTA_URI,
-    issuer: process.env.ISSUER,
+    issuer: process.env.OKTA_ISSUER,
     clientId: process.env.OKTA_CLIENT_ID,
     oktaAPIKey: process.env.OKTA_API_KEY
   }
